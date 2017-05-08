4d4041d05b55107ab6925e60ed40f8ae5a6f9b0b
James Jones <jms.a.jns@gmail.com>
2017-05-08 13:50:07 +0100

    initial commit base pom for server application

---
+++ src/main/java/uk/ac/aston/jonesja1/ers/ServerApplication.java
@@ -0,0 +1,12 @@
+package uk.ac.aston.jonesja1.ers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+}
