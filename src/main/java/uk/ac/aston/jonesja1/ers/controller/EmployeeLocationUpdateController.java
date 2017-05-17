a9b8e4e5f902e2055f02ab01284ccf28fca70d6d
James Jones <jms.a.jns@gmail.com>
2017-05-17 16:42:04 +0100

    send a response to an update request and reject if in calm status

--- src/main/java/uk/ac/aston/jonesja1/ers/controller/EmployeeLocationUpdateController.java
+++ src/main/java/uk/ac/aston/jonesja1/ers/controller/EmployeeLocationUpdateController.java
@@ -4,14 +4,18 @@ package uk.ac.aston.jonesja1.ers.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import uk.ac.aston.jonesja1.ers.model.Employee;
 import uk.ac.aston.jonesja1.ers.model.Location;
 import uk.ac.aston.jonesja1.ers.model.LocationUpdate;
+import uk.ac.aston.jonesja1.ers.model.SystemState;
 import uk.ac.aston.jonesja1.ers.model.request.LocationUpdateRequest;
 import uk.ac.aston.jonesja1.ers.service.EmployeeRiskService;
 import uk.ac.aston.jonesja1.ers.service.employee.EmployeeService;
+import uk.ac.aston.jonesja1.ers.service.state.StateService;
 
 import javax.validation.Valid;
 import java.math.BigDecimal;
@@ -29,23 +33,30 @@ public class EmployeeLocationUpdateController {
     @Autowired
     private EmployeeService employeeService;
 
+    @Autowired
+    private StateService stateService;
+
     @RequestMapping(method = RequestMethod.POST, value = "/", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public void update(@Valid @RequestBody LocationUpdateRequest locationUpdateRequest) {
+    public ResponseEntity update(@Valid @RequestBody LocationUpdateRequest locationUpdateRequest) {
         logger.info("Received Update Request for Employee {}", locationUpdateRequest.getId());
-        Employee employee = employeeService.find(locationUpdateRequest.getId());
-        if (employee == null) {
-            logger.error("Employee {} is not enrolled to use this system.", locationUpdateRequest.getId());
-        } else {
-            LocationUpdate locationUpdate = new LocationUpdate(
-                    employee,
-                    new Location(
-                            BigDecimal.valueOf(locationUpdateRequest.getLongitude()),
-                            BigDecimal.valueOf(locationUpdateRequest.getLatitude())
-                    )
-            );
-            employeeRiskService.calculateEmployeeRiskLevel(locationUpdate);
-            logger.info("Processed Update Request for Employee {}", locationUpdateRequest.getId());
+        if (stateService.currentState() == SystemState.EMERGENCY) {
+            Employee employee = employeeService.find(locationUpdateRequest.getId());
+            if (employee == null) {
+                logger.error("Employee {} is not enrolled to use this system.", locationUpdateRequest.getId());
+            } else {
+                LocationUpdate locationUpdate = new LocationUpdate(
+                        employee,
+                        new Location(
+                                BigDecimal.valueOf(locationUpdateRequest.getLongitude()),
+                                BigDecimal.valueOf(locationUpdateRequest.getLatitude())
+                        )
+                );
+                employeeRiskService.calculateEmployeeRiskLevel(locationUpdate);
+                logger.info("Processed Update Request for Employee {}", locationUpdateRequest.getId());
+                return new ResponseEntity("Accepted", HttpStatus.ACCEPTED);
+            }
         }
+        return new ResponseEntity("System is in CALM state. Will not accept requests.", HttpStatus.METHOD_NOT_ALLOWED);
     }
 
 }
